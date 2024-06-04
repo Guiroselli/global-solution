175b99746b7faab4c98c8965ae0d9e77a5db78b2
Guilherme Damasio Roselli <guilherme.roselli@gmail.com>
2024-06-04 20:27:32 -0300

    JS da aba desenvolvedores arrumada

---
+++ js/scriptdev.js
@@ -0,0 +1,5 @@
+hamburguer_dropdown = document.querySelector(".hamburguer");
+menu = document.querySelector(".menu");
+hamburguer_dropdown.onclick = function () {
+menu.classList.toggle("activate");
+}
